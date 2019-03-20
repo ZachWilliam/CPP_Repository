8e0424a83fda77ce7757f66be53e2815848e526f
tasteslikemetal <47533009+tasteslikemetal@users.noreply.github.com>
2019-03-20 10:44:33 -0400

    inventory weapon update to better naming convention

--- Inventory01/Inventory01/main.cpp
+++ Inventory01/Inventory01/main.cpp
@@ -12,54 +12,54 @@ using namespace std;
 class Weapon
 {
 public:
-	Weapon(string name = "", string type = "", int damageMIN = 0, int damageMAX = 100, bool magic = false);
+	Weapon(string prefix_name = "", string weapon_type = "", int damageMIN = 0, int damageMAX = 100, bool magic = false);
 	void Status();
 	int Attack();
 	string GetName();
 	string GetType();
 private:
-	string m_Name;
-	string m_Type;
+	string m_Prefix_Name;
+	string m_Weapon_Type;
 	int m_DamageMIN;
 	int m_DamageMAX;
 	bool m_Magic;
 
-	static const int NUM_NAMES = 5;
-	static const string NAMES[NUM_NAMES];
+	static const int NUM_PREFIX_NAMES = 5;
+	static const string PREFIX_NAMES[NUM_PREFIX_NAMES];
 
-	static const int NUM_TYPES = 5;
-	static const string TYPES[NUM_TYPES];
+	static const int NUM_WEAPON_TYPES = 5;
+	static const string WEAPON_TYPES[NUM_WEAPON_TYPES];
 };
-Weapon::Weapon(string name, string type, int damageMIN, int damageMAX, bool magic) :
-	m_Name(name), m_Type(type), m_DamageMIN(damageMIN), m_DamageMAX(damageMAX), m_Magic(magic) {}
+Weapon::Weapon(string prefix_name, string weapon_type, int damageMIN, int damageMAX, bool magic) :
+	m_Prefix_Name(prefix_name), m_Weapon_Type(weapon_type), m_DamageMIN(damageMIN), m_DamageMAX(damageMAX), m_Magic(magic) {}
 
 string Weapon::GetName()
 {
-	int namePick = (rand() % NUM_NAMES);
-	m_Name = NAMES[namePick];
-	return m_Name;
+	int namePick = (rand() % NUM_PREFIX_NAMES);
+	m_Prefix_Name = PREFIX_NAMES[namePick];
+	return m_Prefix_Name;
 }
-const string Weapon::NAMES[NUM_NAMES] = { "Smashy Smash",
-"Storm Breaker",
-"Vera",
-"Meat Maker",
-"Other Random Weapon Name" };
+const string Weapon::PREFIX_NAMES[NUM_PREFIX_NAMES] = { "basic",
+"flaming",
+"cold",
+"toxic",
+"shocking" };
 
 string Weapon::GetType()
 {
-	int typePick = (rand() % NUM_TYPES);
-	m_Type = TYPES[typePick];
-	return m_Type;
+	int typePick = (rand() % NUM_WEAPON_TYPES);
+	m_Weapon_Type = WEAPON_TYPES[typePick];
+	return m_Weapon_Type;
 }
-const string Weapon::TYPES[NUM_TYPES] = { "blunt",
-"edged",
-"piercing",
-"ranged",
-"other" };
+const string Weapon::WEAPON_TYPES[NUM_WEAPON_TYPES] = { "sword",
+"axe",
+"mace",
+"spear",
+"bow" };
 
 void Weapon::Status()
 {
-	cout << m_Name << " , " << m_Type << " , " << Attack() << " , " << m_Magic << ".\n";
+	cout << m_Prefix_Name << " , " << m_Weapon_Type << " , " << Attack() << " , " << m_Magic << ".\n";
 }
 int Weapon::Attack()
 {
@@ -71,8 +71,8 @@ int main()
 {
 	srand(static_cast<unsigned int>(time(0)));
 	Weapon weapon1;
-	Weapon weapon2("example", " ", 5, 20, true);
-	Weapon weapon3("", "blunt", 25, 75, true);
+	Weapon weapon2("example", "", 5, 20, true);
+	Weapon weapon3("", "", 25, 75, true);
 
 	weapon1.GetName();
 	weapon1.GetType();
@@ -93,8 +93,9 @@ int main()
 	weapon3.Status();
 	cout << endl;
 
-	cout << "weapon3 after name assign: ";
+	cout << "weapon3 after name and type assign: ";
 	weapon3.GetName();
+	weapon3.GetType();
 	weapon3.Status();
 	cout << endl;
 
