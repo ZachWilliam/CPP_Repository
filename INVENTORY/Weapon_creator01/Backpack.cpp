455eb4358508cf9a5a5e847d014d31d925f8eb46
Curtis <curtis.alderson@triosstudent.com>
2019-04-01 15:33:48 -0400

    PartyInventory - works in main - need actual class names to work

--- INVENTORY/Weapon_creator01/Backpack.cpp
+++ INVENTORY/Weapon_creator01/Backpack.cpp
@@ -44,3 +44,61 @@ void Backpack::Status()
 		cout << endl;
 	}
 }
+
+PartyInventory::PartyInventory() {}
+
+void PartyInventory::SetWeaponsRandom()
+{
+	AddWeapon(-1, -1, -1);
+}
+void PartyInventory::SetArmorRandom()
+{
+	AddArmor(-1, -1, -1);
+}
+
+void PartyInventory::AddWeapon(int x, int y, int z)
+{
+	Weapon tempWeapon;
+	tempWeapon.SetWeapon(x, y, z);
+	INV_WEAPON.push_back(tempWeapon);
+}
+void PartyInventory::AddArmor(int x, int y, int z)
+{
+	Armor tempArmor;
+	tempArmor.SetArmor(x, y, z);
+	INV_ARMOR.push_back(tempArmor);
+}
+
+void PartyInventory::DisplayPartyInventory()
+{
+	cout << "\nWeapons:\n";
+	for (int i = 0; i < INV_WEAPON.size(); ++i)
+	{
+		INV_WEAPON[i].DisplayName();
+		cout << endl;
+	}
+	cout << "\nArmor:\n";
+	for (int i = 0; i < INV_ARMOR.size(); ++i)
+	{
+		INV_ARMOR[i].DisplayName();
+		cout << endl;
+	}
+	cout << endl;
+}
+
+/*
+int PartyInventory::GetID()
+{
+
+	int ID = Backpack::m_Weapon.ID;
+	return ID;
+}
+
+void PartyInventory::SwapEquipedWeapon(int choice)
+{
+	Backpack Weapon() = EQUIPED_WEAPON;
+	INV_WEAPON.push_back(EQUIPED_WEAPON);
+	Backpack Weapon() = INV_WEAPON(choice);
+	//Weapon m_Weapon = PartyInventory::INV_WEAPON[choice];
+}
+*/
