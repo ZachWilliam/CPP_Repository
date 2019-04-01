b0f6e31174d271066254bb101b611e55d38c7fbe
Nicholas Walker <nicholas.walker@triosstudent.com>
2019-04-01 11:45:21 -0400

    Removed Database
    Database has been merged into the Map System section, which at this point is intended as at  least half the body of the overall game.

--- Database/Database/Dialogue.cpp
+++
@@ -1,5 +0,0 @@
-#include "Dialogue.h"
-
-
-Dialogue::Dialogue(int mapID, int speakerID, int textID, string text, string speaker) :
-	m_MapID(mapID), m_SpeakerID(speakerID), m_Text(text), m_TextID(textID), m_Speaker(speaker) {}
