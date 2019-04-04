90d60ec2ea5ede5b529d552033604ff749550878
Kryptoid98 <47532926+Kryptoid98@users.noreply.github.com>
2019-04-04 12:11:10 -0400

    asdasd
    fsdfsdf

---
+++ MapSystem vdont use/MapSystem/Combatant.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include "Stats.h"
+#include "Attack.h"
+#include "AttackManager.h"
+#include "Backpack.h"
+
+using namespace std;
+
+#pragma once
+
+class Combatant
+{
+public:
+    Combatant()
+    {
+        name = "";
+        initiative = 0;
+    }
+    void StartCombat(int in)
+    {
+        initiative = in;
+    }
+    void EndCombat()
+    {
+        initiative = 0;
+    }
+    void SetHP(int MAX_HP)
+    {
+        CurrentHP = MAX_HP;
+    }
+	string name;
+	enum Status {OK, POI, PAR, VEX, CUR, SLP, UNC};
+	enum Stat_Change {UP = 1, NON = 0, DOWN = -1};
+	Status status = OK;
+	Stat_Change OFFENSE = NON;
+	Stat_Change DEFENSE = NON;
+	Stat_Change MOBILITY = NON;
+	Stats CurrentStats = Stats();
+	vector<int> BattleStats = {0,0,0,0,0,0,0,0};
+	vector<float> Resistances = { 1,1,1,1,1,1,1,1,1,1,1 };
+	int initiative;
+	int CurrentHP = 0;
+	int MaxHP = 0;
+	int CurrentMana = 0;
+	bool PlayerControl = false;
+	vector<Attack> CurrentMoves = vector<Attack>(4);
+	Backpack PlayerInventory = Backpack(0, 0, false, 0, 1, 10, 0, 1, 10, 0);
+
+	bool operator==(const Combatant &che) { return name == che.name; };
+};
