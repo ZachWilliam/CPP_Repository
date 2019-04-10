c7a901d5e871d6ae70c12e37b0cb8a6e1aac1e34
Kryptoid98 <kryptoid98@gmail.com>
2019-04-09 18:41:47 -0700

    Update main.cpp

--- MapSystem/MapSystem v12/MapSystem/main.cpp
+++ MapSystem/MapSystem v12/MapSystem/main.cpp
@@ -13,7 +13,9 @@
 using namespace std;
 
 
-
+void DrawTitle(bool p_animate = false);
+void DrawMenu(int p_menuLocation);
+void Credits();
 
 void SetCursor(bool p_isVisible) {
 	HANDLE out = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -39,6 +41,8 @@ void Setup() {
 int main() 
 {
 	SetColorAndBackground();//Default to black/white
+	system("cls");
+
 	srand(static_cast<unsigned int>(time(0)));//Seed
 	Setup();//Set up window and cursor
 
@@ -48,51 +52,187 @@ int main()
 	beast.LoadMonsters();
 	dialogue.LoadText();
 	
+	enum menuState { CONTINUE, NEW_GAME, CREDITS, QUIT };
+	const int MENU_SIZE = 4;
+	int menuLocation = 0;
+	bool isQuitting = false;
 
-	//New GAME
-	//Create character
-	bool inBattle = false;
-	Player PC = Player();
-	CharacterCreator(PC);
+
+	DrawTitle(true);
+
+	while (!isQuitting) {
+		DrawTitle();
+		DrawMenu(menuLocation);
+		switch (_getch()) {
+			case 72: {//UP
+				if (menuLocation == 0) menuLocation = MENU_SIZE - 1;
+				else menuLocation--;
+				break;
+			}
+			case 80: {//DOWN
+				if (menuLocation == MENU_SIZE - 1) menuLocation = 0;
+				else menuLocation++;
+				break;
+			}
+			case 'z': {
+				switch (menuLocation) {
+					case CONTINUE: { //Continue
+						menuLocation = CONTINUE;
+						break;
+					}
+					case NEW_GAME: { // New Gane
+						menuLocation = CONTINUE;
+						//Create character
+						bool inBattle = false;
+						Player PC = Player();
+						CharacterCreator(PC);
+						system("cls");
+						//Set up party and inventory
+						Party TheGroup = Party(PC);
+						TheGroup.GenPartyFromClass(PC);
+						TheGroup.DisplayParty();
+						PartyInventory Inventory(TheGroup.Container[1].PlayerInventory, TheGroup.Container[3].PlayerInventory, TheGroup.Container[5].PlayerInventory);
+						//Show party overview
+						Inventory.DisplayPartyInventory();
+						_getch();
+						system("cls");
+
+						//Generate Attacks
+						for (size_t i = 0; i < TheGroup.Container.size(); i++)
+						{
+							if (TheGroup.Container[i].name != "NULL_NAME")
+							{
+								TheGroup.Container[i].GenerateAttacks();
+							}
+						}
+
+						SetColorAndBackground();
+
+						//Create and start game loop
+						GManager.gameState = GManager.PLAY;
+						MapMain mapMain(dialogue, beast, TheGroup, Inventory);
+						mapMain.Setup(1, 0, 0);
+						mapMain.main();
+
+						system("cls");
+
+						//Play either game over or the game ending
+						EndGame endGame(dialogue);
+						break;
+					}
+					case CREDITS: { // Credits
+						Credits();
+						menuLocation = CONTINUE;
+						break;
+					}
+					case QUIT: { // Quit
+						isQuitting = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+void DrawTitle(bool p_animate) {
+	int animateSpeed = 1;
+	if (!p_animate) animateSpeed = 0;
+	SetColorAndBackground(BLACK, LIGHTCYAN);
+
+	GoToXY(0, 0);
+	string title = R"(
+                  ____     ________   ________     ____       ____    __   ___       
+                 (    )   (___  ___) (___  ___)   (    )     / ___)  () ) / __)      
+                 / /\ \       ) )        ) )      / /\ \    / /      ( (_/ /         
+                ( (__) )     ( (        ( (      ( (__) )  ( (       ()   (          
+                 )    (       ) )        ) )      )    (   ( (       () /\ \         
+                /  /\  \     ( (        ( (      /  /\  \   \ \___   ( (  \ \        
+               /__(  )__\    /__\       /__\    /__(  )__\   \____)  ()_)  \_\       
+                                                                                     
+                                    ____        __      _                            
+                                   / __ \      /  \    / )                           
+                                  / /  \ \    / /\ \  / /                            
+                                 ( ()  () )   ) ) ) ) ) )                            
+                                 ( ()  () )  ( ( ( ( ( (                             
+                                  \ \__/ /   / /  \ \/ /                             
+                                   \____/   (_/    \__/                              
+                                                                                     
+        __   ___   ______       ____       ____    __   ___    _____      __      _  
+       () ) / __) (   __ \     (    )     / ___)  () ) / __)  / ___/     /  \    / ) 
+       ( (_/ /     ) (__) )    / /\ \    / /      ( (_/ /    ( (__      / /\ \  / /  
+       ()   (     (    __/    ( (__) )  ( (       ()   (      ) __)     ) ) ) ) ) )  
+       () /\ \     ) \ \  _    )    (   ( (       () /\ \    ( (       ( ( ( ( ( (   
+       ( (  \ \   ( ( \ \_))  /  /\  \   \ \___   ( (  \ \    \ \___   / /  \ \/ /   
+       ()_)  \_\   )_) \__/  /__(  )__\   \____)  ()_)  \_\    \____\ (_/    \__/    
+                                                                                  
+                       <~~~~~Press "Z" On One Of The Following~~~~~>
+	)";
+
+	for (int i = 0; i < title.size(); i++)
+	{
+		cout << title[i];
+		Sleep(animateSpeed);
+	}
+}
+
+void Credits() {
 	system("cls");
-	//Set up party and inventory
-	Party TheGroup = Party(PC);
-	TheGroup.GenPartyFromClass(PC);
-	TheGroup.DisplayParty();
-	PartyInventory Inventory(TheGroup.Container[1].PlayerInventory, TheGroup.Container[3].PlayerInventory, TheGroup.Container[5].PlayerInventory);
-	//Show party overview
-	Inventory.DisplayPartyInventory();
+	SetColorAndBackground(BLACK, LIGHTCYAN);
+
+	//Use center word here
+	cout << endl << "Created By VGDD Class of 2019" << endl;
+	cout << endl;
+	cout << R"(
+                                      +-+-+-+-+-+
+                                      |S|c|o|t|t|
+                                      +-+-+-+-+-+
+)" << endl
+<< R"(
+                                       +-+-+-+-+
+                                       |Z|a|c|h|
+                                       +-+-+-+-+
+)" << endl
+<< R"(
+                                  +-+-+-+-+-+-+-+-+-+
+                                  |S|e|b|a|s|t|i|a|n|
+                                  +-+-+-+-+-+-+-+-+-+
+)" << endl
+<< R"(
+                                     +-+-+-+-+-+-+
+                                     |C|u|r|t|i|s|
+                                     +-+-+-+-+-+-+
+)" << endl
+<< R"(
+                                       +-+-+-+-+
+                                       |N|i|c|k|
+                                       +-+-+-+-+
+)" << endl
+<< R"(
+                                      +-+-+-+-+-+
+                                      |A|a|r|o|n|
+                                      +-+-+-+-+-+
+)" << endl;
+
+	//Use Centerword here
+	cout << "Press any key to continue..." << endl;
 	_getch();
 	system("cls");
 
-	//Generate Attacks
-	for (size_t i = 0; i < TheGroup.Container.size(); i++)
+}
+
+void DrawMenu(int p_menuLocation) {
+	const int MENU_SIZE = 4;
+	const string MENU[MENU_SIZE] = { "Continue", "New Game", "Credits", "Quit" };
+
+	GoToXY(28, 0);
+	for (int i = 0; i < MENU_SIZE; i++)
 	{
-		if (TheGroup.Container[i].name != "NULL_NAME")
-		{
-			TheGroup.Container[i].GenerateAttacks();
-		}
+		if (i == p_menuLocation) SetColorAndBackground(BLACK, LIGHTCYAN);
+		else SetColorAndBackground(BLACK, CYAN);
+		//Use Center Word here
+		cout << MENU[i] << endl;
 	}
-	//NEW GAME
-
-	   	 
-
-	//Game Loop
-
-
-	//Create and start game loop
-	GManager.gameState = GManager.PLAY;
-	MapMain mapMain(dialogue, beast, TheGroup, Inventory);
-	mapMain.Setup(6, 0, 0);
-	mapMain.main();
-
-	system("cls");
-
-	//Game Loop
-
-	//Play either game over or the game ending
-	EndGame endGame(dialogue);
-	
-
-	return 0;
 }
